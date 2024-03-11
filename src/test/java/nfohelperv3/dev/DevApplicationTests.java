d9ff27030ea47a0c889eda3b6a82a8ff0c03de64
Linhebin <846239791@qq.com>
2024-03-12 02:27:35 +0800

    init

---
+++ src/test/java/nfohelperv3/dev/DevApplicationTests.java
@@ -0,0 +1,21 @@
+package nfohelperv3.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.swing.*;
+import javax.swing.filechooser.FileSystemView;
+import java.io.File;
+
+@SpringBootTest
+class DevApplicationTests {
+
+
+    @Test
+    void contextLoads() {
+
+
+
+    }
+
+}
