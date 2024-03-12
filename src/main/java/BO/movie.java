197ff8c5f82c67a4c35030a57bc07c264a25bee9
Linhebin <846239791@qq.com>
2024-03-13 01:10:30 +0800

    *更换XML解析框架

--- src/main/java/BO/movie.java
+++ src/main/java/BO/movie.java
@@ -1,6 +1,6 @@
 package BO;
 
-import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.;
 import com.thoughtworks.xstream.annotations.XStreamAsAttribute;
 import com.thoughtworks.xstream.annotations.XStreamConverter;
 import com.thoughtworks.xstream.converters.basic.StringConverter;
@@ -19,67 +19,67 @@ public class movie implements Serializable {
     /**
      * 标题
      */
-    @XStreamAlias("title")
+    @("title")
     private String title;
     /**
      * 评分
      */
-    @XStreamAlias("rating")
+    @("rating")
     private Double rating;
     /**
      * 简介
      */
-    @XStreamAlias("plot")
+    @("plot")
     private String plot;
     /**
      * runTime
      */
-    @XStreamAlias("runtime")
+    @("runtime")
     private Integer runtime;
     /**
      * mpaa
      */
-    @XStreamAlias("mpaa")
+    @("mpaa")
     private String mpaa;
     /**
      * 标签 List
      */
-    @XStreamAlias("genre")
+    @("genre")
     private List<String> genre;
     /**
      * tag List
      */
-    @XStreamAlias("tag")
+    @("tag")
     private List<String> tag;
     /**
      * 国家
      */
-    @XStreamAlias("country")
+    @("country")
     private String country;
     /**
      * 导演
      */
-    @XStreamAlias("director")
+    @("director")
     private String director;
     /**
      * 发行时间
      */
-    @XStreamAlias("premiered")
+    @("premiered")
     private Date premiered;
     /**
      * 工作室
      */
-    @XStreamAlias("studio")
+    @("studio")
     private String studio;
     /**
      * 地址 trailer
      */
-    @XStreamAlias("trailer")
+    @("trailer")
     private String trailer;
     /**
      * UniqueId
      */
-    @XStreamAlias("uniqueid")
+    @("uniqueid")
     private List<UniqueID> uniqueid;
     @Getter
     @Setter
